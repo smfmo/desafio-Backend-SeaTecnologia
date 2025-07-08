581c708b4677c3d2a71d7699855322f5b0cdb926
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-07-08 12:37:56 -0300

    Commit inicial: preparação do ambiente e criação de pacotes da aplicação.

---
+++ backend/src/main/java/desafio/seatecnologia/backend/RegistroClientesApplication.java
@@ -0,0 +1,13 @@
+package desafio.seatecnologia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroClientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/desafio/seatecnologia/backend/RegistroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.seatecnologia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
